891d7f26ee2a91d5081c9c3cb8afe7d38f0eeba5
isaac-martin <isaac@weareplayground.co>
2017-07-20 19:40:58 -0400

    Pushed Changes To Live

--- styles/custom/v1/js/main.js
+++ styles/custom/v1/js/main.js
@@ -19,30 +19,22 @@ $(document).ready(function() {
 	 	var defaultSubmitText = $submit.val();
 
 	 	$.ajax({
-	 		url: '//formspree.io/contact@isaacmartin.co',
+			// 	url: '//formspree.io/contact@isaacmartin.co',
 	 		method: 'POST',
 	 		data: $(this).serialize(),
 	 		dataType: 'json',
 	 		beforeSend: function() {
 	 			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
-	 			$submit.attr('disabled', true).val('Sending message…');
 	 		},
 	 		success: function(data) {
 	 			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
-	 			$submit.val('Message sent!');
-	 			setTimeout(function() {
-	 				//$('.alert--success').remove();
-	 				$submit.attr('disabled', false).val(defaultSubmitText);
-	 			}, 5000);
+				$contactForm.fadeOut(3000);
+				console.log('Message Sent');
 	 		},
 	 		error: function(err) {
-	 			//$contactForm.find('.alert--loading').hide();
 	 			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
-	 			$submit.val('Ops, there was an error.');
-	 			setTimeout(function() {
-	 				//$('.alert--error').remove();
-	 				$submit.attr('disabled', false).val(defaultSubmitText);
-	 			}, 5000);
+	 			$('.alert--error').remove();
+				console.log('Ops, there was an error');
 	 		}
 	 	});
 	 });
