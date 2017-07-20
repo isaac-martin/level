9963aa28fccd0deffd49d1942bf3654fe49b5d85
isaac-martin <isaac@weareplayground.co>
2017-07-20 19:33:58 -0400

    Fixing Form

--- styles/custom/v1/js/main.js
+++ styles/custom/v1/js/main.js
@@ -24,11 +24,11 @@ $(document).ready(function() {
 	 		data: $(this).serialize(),
 	 		dataType: 'json',
 	 		beforeSend: function() {
-	 			//$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+	 			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
 	 			$submit.attr('disabled', true).val('Sending message…');
 	 		},
 	 		success: function(data) {
-	 			//$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+	 			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
 	 			$submit.val('Message sent!');
 	 			setTimeout(function() {
 	 				//$('.alert--success').remove();
@@ -37,7 +37,7 @@ $(document).ready(function() {
 	 		},
 	 		error: function(err) {
 	 			//$contactForm.find('.alert--loading').hide();
-	 			//$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+	 			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
 	 			$submit.val('Ops, there was an error.');
 	 			setTimeout(function() {
 	 				//$('.alert--error').remove();
